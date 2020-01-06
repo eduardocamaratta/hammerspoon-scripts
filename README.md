090561489af3b9af8bad0ca609293ed491180a43
Eduardo Camaratta <Eduardo.Camaratta@erasys.de>
2020-01-06 15:01:01 +0100

    Script imported

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# hammerspoon-scripts
+# Hammerspoon Scripts
+
 My personal Hammerspoon scripts
+
+
+## Instructions
+
+- Install Hammerspoon (Duh!)
+- Configure it to launch at login and give it accessibility access
+- Copy the script to ~/.hammerspoon (final path: ~/.hammerspoon/init.lua)
+- Reload the configuration in Hammerspoon
+
+
+## Features
+
+The current script contains shortcuts to move a window to the next display (ctrl + alt + cmd + n), maximize (ctrl + alt + cmd + up), and split horizontally - to the left (ctrl + alt + cmd + left) and to the right (ctrl + alt + cmd + right).
+
+
+## Acknowledgments
+
+[andrewk](https://stackoverflow.com/a/55778307) - Author of the script which allows to move a window to the next display keeping relative position and size
+
+
+## Author
+
+[Eduardo Camaratta](https://github.com/eduardocamaratta)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/eduardocamaratta/bashrc/blob/master/LICENSE) file for details.
---
+++ init.lua
@@ -0,0 +1,33 @@
+hs.window.animationDuration = 0
+units = {
+  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+}
+
+-- Credits to https://stackoverflow.com/a/55778307
+function moveWindowToNextDisplay()
+  -- Get the focused window, its window frame dimensions, its screen frame dimensions,
+  -- and the next screen's frame dimensions.
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedScreenFrame = focusedWindow:screen():frame()
+  local nextScreenFrame = focusedWindow:screen():next():frame()
+  local windowFrame = focusedWindow:frame()
+
+  -- Calculate the coordinates of the window frame in the next screen and retain aspect ratio
+  windowFrame.x = ((((windowFrame.x - focusedScreenFrame.x) / focusedScreenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x)
+  windowFrame.y = ((((windowFrame.y - focusedScreenFrame.y) / focusedScreenFrame.h) * nextScreenFrame.h) + nextScreenFrame.y)
+  windowFrame.h = ((windowFrame.h / focusedScreenFrame.h) * nextScreenFrame.h)
+  windowFrame.w = ((windowFrame.w / focusedScreenFrame.w) * nextScreenFrame.w)
+
+  -- Set the focused window's new frame dimensions
+  focusedWindow:setFrame(windowFrame)  
+end
+
+mash = { 'ctrl', 'alt', 'cmd' }
+
+hs.hotkey.bind(mash, 'Left',  function() hs.window.focusedWindow():move(units.left50,  nil, true) end)
+hs.hotkey.bind(mash, 'Right', function() hs.window.focusedWindow():move(units.right50, nil, true) end)
+hs.hotkey.bind(mash, 'Up',    function() hs.window.focusedWindow():move(units.maximum, nil, true) end)
+
+hs.hotkey.bind(mash, 'n', moveWindowToNextDisplay)
